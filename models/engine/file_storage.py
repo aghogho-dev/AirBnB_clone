5c7bcfe9469c1e2e7164fbf9d951e60156e9a5fa
Aghogho <monorienaghogho@gmail.com>
2023-02-11 17:13:08 -0800

    5. Store first ob

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,30 +14,26 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
     
-    @classmethod
-    def all(cls):
+    def all(self):
         """Returns __objects"""
-        return cls.__objects
+        return self.__objects
 
-    @classmethod
-    def new(cls, obj):
+    def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         name = obj.__class__.__name__
-        cls.__objects["{}.{}".format(name, obj.id)] = obj
+        self.__objects["{}.{}".format(name, obj.id)] = obj
 
-    @classmethod
-    def save(cls):
+    def save(self):
         """erializes __objects to the JSON file (path: __file_path)"""
-        my_dict = cls.__objects
+        my_dict = self.__objects
         new_dict = {key: my_dict[key].to_dict() for key in my_dict]
-        with open(cls.__file_path, "w") as file:
+        with open(self.__file_path, "w") as file:
             json.dump(new_dict, file)
 
-    @classmethod
     def reload(cls):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists."""
         try:
-            with open(cls.__file_path) as file:
+            with open(self.__file_path) as file:
                 my_dict = json.load(file)
                 for v in my_dict.values():
                     name_cls = v['__class__']
